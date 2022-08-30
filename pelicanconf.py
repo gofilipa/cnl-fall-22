d8f10ffbb150d0a9b29aefae06609dca353de945
Filipa Calado <gofilipa@gmail.com>
2022-08-30 01:29:36 -0400

    trying to change relative link on menu

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,7 +40,7 @@ DISPLAY_PAGES_ON_MENU = False
 
 MENUITEMS = (
     ('Syllabus', '/pages/syllabus.html'),
-    ('Course Schedule', '/pages/course_schedule.html'),
+    ('Course Schedule', 'cnl-fall-22/pages/course_schedule.html'),
     ('Homework', '/pages/homework.html'),
     ('Submit Homework', 'https://drive.google.com/drive/folders/1zukMmnzy3XW3zuz7o6UQL2wRtaV2UBkk?usp=sharing'),
     ('Class Notes', 'https://docs.google.com/document/d/1LYqjvlBocqFYnEMH3uzXE6r_WK2KfaDFB8swgh33K9M/edit?usp=sharing')
